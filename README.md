92be4e9d6fa81341dc116592d3dc9dad752f4e04
sirabhishek <rathore.abhishek656@gmail.com>
2021-02-06 03:38:24 +0530

    Updated week 1 file

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # python-basics-coursera
-This is practice repo made for keeping track of my own skills in python and reaching the end of the course. if you wish to check it out feel free.
+-This is practice repo made for keeping track of my own skills in python and reaching the end of the course. if you wish to check it out feel free.
+
+-This is a paid course that is offered by coursera.
+
+-It's linked with the Michigan University.
+
---
+++ Week 1 The Way of Programmer.py
@@ -0,0 +1,25 @@
+#starts from Debugging 
+# 1st Video 
+
+
+
+#Sytax error 
+
+print("hey"                 #this is an example of Sytax error 
+                            #this gives us an EOF error which means "End Of The File" 
+
+#Runtime error
+
+print(15/0)                 #this is an example of runtime error
+                            #this gives an runtime error which says it's not ideal to divide by zero
+
+
+#Semantic error
+
+print("what is half a percentage-" 1/2)       #this is an example of semanic error 
+                                              #this gives us an error which is that it should give us 50% but instead it will give is 0.5
+
+#End of Error Section
+
+
+
